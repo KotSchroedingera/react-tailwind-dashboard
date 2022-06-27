417f9e1f45df87ef72cc019695f51a717ca30683
kotschroedingera <79150323671@ya.ru>
2022-06-27 14:39:20 +0300

    3 pages (tables)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ category, title, ...props }) => {
+  return (
+    <div className='mr-10' {...props}>
+      <p className='text-gray-400'>{category}</p>
+      <p className='text-3xl font-extrabold tracking-tight text-slate-900'>
+        {title}
+      </p>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  Edit,
+  Search,
+  Inject,
+  Toolbar,
+} from '@syncfusion/ej2-react-grids';
+
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
-  return <div>Employees</div>;
+  return (
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header className='mb-10' category='Page' title='Employees' />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        width='auto'
+        toolbar={['Search']}>
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Employees;
